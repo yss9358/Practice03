7b4688cbd60aa70dfa768695ba58558e85b9958d
yss9358 <93y2s@naver.com>
2023-12-28 19:22:30 +0900

    0809찾아보기

---
+++ src/com/javaex/practice/Ex09.java
@@ -0,0 +1,19 @@
+package com.javaex.practice;
+
+public class Ex09 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		
+		[Ex09.java]
+				
+		다음과 같이 출력하세요
+		
+		*/
+		
+		
+	}
+
+}
